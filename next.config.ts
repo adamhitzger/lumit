5b25da6002ec8b8e7a7c6ad4ed8cda85479bf678
adamhitzger <adam.hitzger@icloud.com>
2025-07-15 20:25:44 +0200

    first commit

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,26 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-  /* config options here */
+    images: {
+    remotePatterns: [
+      {
+        protocol: 'https',
+        hostname: 'cdn.sanity.io',
+      },
+      {
+        hostname: "lh3.googleusercontent.com",
+        protocol: "https"
+      },
+      {
+        hostname: "images.unsplash.com",
+        protocol: "https"
+      },
+       {
+        hostname: "d19-a.sdn.cz",
+        protocol: "https"
+      }
+    ],
+  },
 };
 
 export default nextConfig;
